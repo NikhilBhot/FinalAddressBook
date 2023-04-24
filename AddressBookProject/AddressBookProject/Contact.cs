7ad9fbcef74aae20a167037e9ee7c3db0fcbf0d9
unknown <bhotnikhil25@gmail.com>
2023-04-25 00:45:09 +0530

    [Nikhil]-[200~Ability to create a Contacts in Address Book with first and last names,

---
+++ AddressBookProject/AddressBookProject/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProject
+{
+    public class Contact
+    {
+        //UC-01
+        /*
+         * Ability to create a Contacts in Address Book with first and last names, 
+         * address, city, state, zip, phone number and email...
+         */
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+    }
+}
+
---
+++ AddressBookProject/AddressBookProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come To AddressBook Project");
+        }
+    }
+}
